6a8193af784caacc01e275371eadf76395142702
Achieng Mary <achieng75mary@gmail.com>
2023-08-28 05:53:18 -0700

    chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,23 +1,19 @@
+#include "_putchar.c"
 #include "main.h"
 
 /**
- * print_chessboard - Prints a chessboard.
- *
- * @a: The chessboard to print.
- *
- * Return: Nothing.
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
-	int row, col;
+	int i, j;
 
-	for (row = 0; row < 8; row++)
+	for (i = 0; i < 8; i++)
 	{
-		for (col = 0; col < 8; col++)
-		{
-			_putchar(a[row][col]);
-		}
-
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
 		_putchar('\n');
 	}
 }
