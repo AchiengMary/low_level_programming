41770015cd70f0e599d9cc9c2cbc91ee3c79ee20
Achieng Mary <achieng75mary@gmail.com>
2023-08-31 02:09:10 -0700

    language in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *  main -Entry point
+ * main-entry point
  *
- *  Return: Always 0 (Success)
+ * Return: always 0(success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+	return(0);
 }
