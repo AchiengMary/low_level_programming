9ae6dd8860059489324a78fb8531a8de87ac4f7b
Achieng Mary <116246508+AchiengMary@users.noreply.github.com>
2023-09-04 11:57:52 +0300

    Update 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*main - malloc and free
+*
+*Return: 0.
+*/
+int main(void)
+{
+  char *arrays_allocation(unsigned int size, char c);
+  arrays_allocation = malloc(sizeof(char)*4);
+  arrays_allocation[0] = 'G';
+  arrays_allocation[1] = 'O';
+  arrays_allocation[2] = 'H';
+  arrays_allocation[3] = '\0';
+  printf("%s\n" ,arrays_allocation);
+  return (0);
+/*achieng*/
+}
