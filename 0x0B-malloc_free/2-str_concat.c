b1ef017fef7ae386f84c60c5344f43e6cafb3f9b
Achieng Mary <achieng75mary@gmail.com>
2023-09-07 01:37:03 -0700

    he who is not courageous enough to take risks will acomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,13 +18,13 @@ char *str_concat(char *s1, char *s2)
 	if (s1 != NULL)
 	{
 		i = 0;
-		while (s1[i++] != '\0'
+		while (s1[i++] != '\0')
 				len1++;
 	}
 	if (s2 != NULL)
 	{
 		i = 0;
-		while (s2[i++] != '\0'
+		while (s2[i++] != '\0')
 				len2++;
 	}
 
