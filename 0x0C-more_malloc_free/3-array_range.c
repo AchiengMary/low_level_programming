3dba07247c2069e04595df35375470190716fb9e
Achieng Mary <116246508+AchiengMary@users.noreply.github.com>
2023-09-08 12:54:09 +0300

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,17 +11,17 @@
 
 int *array_range(int min, int max)
 {
-  int *arr_loc;
+  int *arr;
   int x, size;
 
   if (min > max)
     return (NULL);
   size = max - min + 1;
-  arr_loc = malloc(sizeof(int) * size);
-  if (arr_loc == NULL)
+  arr = malloc(sizeof(int) * size);
+  if (arr == NULL)
     return (NULL);
   for (x = 0; min <= max; x++)
-    arr_loc[x] = min++;
-    return (arr_loc);
+    arr[x] = min++;
+    return (arr);
   /*achieng*/
 }
