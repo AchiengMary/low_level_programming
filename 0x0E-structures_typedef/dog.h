6d109bf5ce667fd384c533b1689b98010809426b
Achieng Mary <116246508+AchiengMary@users.noreply.github.com>
2023-09-12 01:03:46 +0300

    Update dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifdef DOG
+#define DOG
+
+/**
+*struct dog - class of the dog
+*
+*@name: the dogs name
+*
+*@age: the dogs age
+*
+*@owner: the dogs owner
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif/*DOG*/
