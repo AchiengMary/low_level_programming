5bd0d9366893f6180e68a5fcbb02b7db69a7c89a
Achieng Mary <116246508+AchiengMary@users.noreply.github.com>
2023-09-12 02:33:11 +0300

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG
+#define DOG
+
+/**struct_dog - a new type defined dog
+*
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*/
+
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
