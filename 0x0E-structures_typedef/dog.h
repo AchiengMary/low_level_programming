c45858595127853b3549af55453fd938c8e40b4c
Achieng Mary <achieng75mary@gmail.com>
2023-09-14 08:52:09 -0700

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,16 +2,29 @@
 #define DOG
 
 /**
-*struct dog - a new type defined dog
-*
-*@name: name of dog
-*@age: age of dog
-*@owner: owner of dog
-*/
+ * struct dog - dog class
+ *
+ * @name: Name of the dog
+ *
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
 
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif
