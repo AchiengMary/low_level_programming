dd1d88914c9004802d1590424ed4e31f9a65ec9b
Achieng Mary <achieng75mary@gmail.com>
2023-09-14 09:55:03 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,43 +1,47 @@
+#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
 /**
- * *str_concat - conactenates two strings
+ * str_concat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
  *
- * @s1: string one to concatenate
- * @s2: string two to conacatenate
- *
- * Return: fail(NULL), Success(string)
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *answer;
-	int i = 0, j = 0, len1 = 0, len2 = 0, len = len1 + len2;
+	int i, j, len1, len2, len;
+	char *result;
+
+	len1 = len2 = 0;
 
 	if (s1 != NULL)
 	{
 		i = 0;
 		while (s1[i++] != '\0')
-				len1++;
+			len1++;
 	}
+
 	if (s2 != NULL)
 	{
 		i = 0;
 		while (s2[i++] != '\0')
-				len2++;
+			len2++;
 	}
 
-	answer = (char *)malloc(sizeof(char) * (len + 1));
-	if (answer == NULL)
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
 		return (NULL);
-	for (i = 0; i < len1; i++)
-		answer[i] = s1[i];
-	for (j = 0; j < len2; j++, i++)
-		answer[i] = s2[j];
-	answer[len] = '\0';
-	return (answer);
-	/*achieng*/
-}
 
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
+}
