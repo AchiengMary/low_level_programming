cf3d1db1c3a0799fbe298188f11a0a99ba642490
Achieng Mary <achieng75mary@gmail.com>
2023-09-14 08:58:41 -0700

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,16 +2,17 @@
 #include "dog.h"
 
 /**
-* free_dog - function that frees dogs
-*@d: points to the dog to be freed
-*
-*/
+ * free_dog - frees dogs
+ * @d: pointer to dog to free
+ *
+ * Return: void
+ */
 void free_dog(dog_t *d)
 {
-  if (d)
-  {
-    free(d->name);
-    free(d->owner);
-    free(d);
-  }
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
