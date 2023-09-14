8cac83fdd1d54e6b6d99dbca6babf3b3a917a5b7
Achieng Mary <achieng75mary@gmail.com>
2023-09-13 18:42:05 -0700

    what is my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -  prints a name
+ * @name: name to be printed
+ * @f: function of name
+ *
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+
+		f(name);
+}
