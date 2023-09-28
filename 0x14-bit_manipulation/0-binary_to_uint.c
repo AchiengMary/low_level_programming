45af2a9c36d9d84d64d3be8b6f35d353032aacec
Achieng Mary <achieng75mary@gmail.com>
2023-09-28 03:24:07 -0700

    binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,12 +10,14 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int result = 0;
+	int i;
+
 	if (b == NULL)
 	{
 		return 0;
 	}
-	unsigned int result = 0;
-	for (int i = strlen(b) - 1; i >= 0; i--)
+	for (i = strlen(b) - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 		{
